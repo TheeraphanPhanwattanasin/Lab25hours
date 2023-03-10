b6e258f9a65cda5ffee5131f7586635845bb54f9
b <b>
2023-03-10 22:09:55 +0700

    25.2 25.3 completed

--- lab25_3.cpp
+++ lab25_3.cpp
@@ -1,8 +1,15 @@
 #include<iostream>
-
+#include <set>
 using namespace std;
 
-//Write function count() here.
+int count(int data[], int N){
+set<int> mydata;
+for(int i=0; i<N; i++)
+mydata.insert(data[i]);
+set<int>::iterator j;
+ j = mydata.end();
+return *j;
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
